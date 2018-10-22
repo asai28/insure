b50e59e2c753f1c968a2070a45474b594c1b52ed
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-10-22 09:34:32 -0700

    Changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export default{
 
-    numAPI : axios.get("https://apilayer.net/api/validate")
+    numAPI : axios.get("https://apilayer.net/api/validate?access_key=4bc1e72596e3c0a64ec10002844133f5")
 }
 
 
-//http://apilayer.net/api/validate
