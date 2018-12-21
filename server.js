66fc7a0cf28d6d8106a460155905002f911e26e0
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-12-21 16:18:25 -0700

    Removing connection to front end for now

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const path = require('path')
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+// app.use(express.static(path.join(__dirname, 'client/build')))
+// // Anything that doesn't match the above, send back index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
 
 var db = require("./models");
 
