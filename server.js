7900dd8606cae8148b774fb05cb47f6b02583a33
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-12-26 21:43:27 -0800

    Changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-const path = require('path')
 // Serve static files from the React frontend app
 // app.use(express.static(path.join(__dirname, 'client/build')))
 // // Anything that doesn't match the above, send back index.html
