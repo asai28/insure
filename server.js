67c75c4942ebc58fe5b3fbf667c300ded9d1d326
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-12-27 11:39:55 -0800

    All changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static("app/build"));
+app.use(express.static("client/build"));
 
 // Serve static files from the React frontend app
 // app.use(express.static(path.join(__dirname, 'client/build')))
