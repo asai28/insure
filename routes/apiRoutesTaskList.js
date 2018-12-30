d3dd5256b4dd6e1b46fea73d51d1256a768ac40b
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-12-30 15:37:53 -0800

    try this

--- routes/apiRoutesTaskList.js
+++ routes/apiRoutesTaskList.js
@@ -1,12 +1,11 @@
 var db = require("../models");
 
 // // This may be confusing but here Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
 // // sequelize (lowercase) references our connection to the DB.
 // var sequelize = require("../models");
 
-const Op = Sequelize.Op;
 module.exports = function(app) {
+const Op = db.Sequelize.Op;
    app.get("/api/tasklist/:emp", function(req, res){
         db.TaskList.findAll({
             where: {
