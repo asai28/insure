2acc4f54e4c9429885030db4c75824aa26ea9aee
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2019-01-04 13:07:02 -0700

    add build

---
+++ client/build/precache-manifest.a71de764f28d756a1726df6824f85e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "922b9e55d4665d87e59f",
+    "url": "/static/js/main.922b9e55.chunk.js"
+  },
+  {
+    "revision": "f2c5511f1fee2c1efa1e",
+    "url": "/static/js/1.f2c5511f.chunk.js"
+  },
+  {
+    "revision": "922b9e55d4665d87e59f",
+    "url": "/static/css/main.82796585.chunk.css"
+  },
+  {
+    "revision": "f2c5511f1fee2c1efa1e",
+    "url": "/static/css/1.127052e1.chunk.css"
+  },
+  {
+    "revision": "2d15b74419975e1cf4e50d738c40e406",
+    "url": "/index.html"
+  }
+];
